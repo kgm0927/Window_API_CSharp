d6f71c9111f06674f63f50f51a1581fb95f1d6d0
kgm0927 <initial929@gmail.com>
2024-06-21 19:18:45 +0900

    10주차 업로드

---
+++ 객체지향 윈도우즈 프로그래밍 10주차/MyDll/MyDll.cpp
@@ -0,0 +1,20 @@
+//* 직접 엑스포트하기
+extern "C" __declspec(dllexport) int AddInteger(int a, int b)
+{
+	return a+b;
+}
+extern "C" __declspec(dllexport) int SubInteger(int a, int b)
+{
+	return a - b;
+}
+//*/
+
+/* 헤더 파일로 엑스포트하기
+#define DLLEXPORT
+#include "MyDll.h"
+
+extern "C" MYDLLTYPE int AddInteger(int a, int b)
+{
+	return a+b;
+}
+//*/
---
+++ 객체지향 윈도우즈 프로그래밍 10주차/TCPClient-PortScanner/TCPClient.cpp
@@ -0,0 +1,94 @@
+#include <winsock2.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define BUFSIZE 512
+
+// 소켓 함수 오류 출력 후 종료
+void err_quit(char *msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage( 
+		FORMAT_MESSAGE_ALLOCATE_BUFFER|
+		FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(-1);
+}
+
+// 소켓 함수 오류 출력
+void err_display(char *msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage( 
+		FORMAT_MESSAGE_ALLOCATE_BUFFER|
+		FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	printf("[%s] %s", msg, (LPCTSTR)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+// 사용자 정의 데이터 수신 함수
+int recvn(SOCKET s, char *buf, int len, int flags)
+{
+	int received;
+	char *ptr = buf;
+	int left = len;
+
+	while(left > 0){
+		received = recv(s, ptr, left, flags);
+		if(received == SOCKET_ERROR) 
+			return SOCKET_ERROR;
+		else if(received == 0) 
+			break;
+		left -= received;
+		ptr += received;
+	}
+
+	return (len - left);
+}
+
+int main(int argc, char* argv[])
+{
+	int retval;
+
+	// 윈속 초기화
+	WSADATA wsa;
+	if(WSAStartup(MAKEWORD(2,2), &wsa) != 0)
+		return -1;
+
+	// socket()
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	if(sock == INVALID_SOCKET) err_quit("socket()");	
+	
+	// connect()
+	SOCKADDR_IN serveraddr;
+	ZeroMemory(&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	//serveraddr.sin_port = 9000;
+	serveraddr.sin_addr.s_addr = inet_addr("192.168.0.3");
+
+	for (int portnum = 79; portnum < 82; portnum++){
+		printf("port num : %d Test!!!", portnum);
+		serveraddr.sin_port = htons(portnum);
+		retval = connect(sock, (SOCKADDR *)&serveraddr, sizeof(serveraddr)); // 소켓을 만들어서 새로 해 주는 것을 한번 고민해 보아야 한다.
+		if (retval != SOCKET_ERROR)
+		{
+			printf("port num : %d \n", portnum);
+			closesocket(sock);
+		}
+			
+		else err_display("connect()");
+	}
+			
+	
+
+	// 윈속 종료
+	WSACleanup();
+	return 0;
+}
